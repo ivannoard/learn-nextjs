c8f647bb6376553e192d77599d7ba939d417cda8
ivan <ivannovaa.24@gmail.com>
2022-08-07 15:45:50 +0700

    login register page

---
+++ pages/auth/login.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+export default function Login() {
+
+  const [fields, setFields] = useState({
+    email: '',
+    password: ''
+  })
+
+  const [status, setStatus] = useState('normal')
+
+  const handleChange = (e) => {
+    const inputName = e.target.getAttribute('name')
+    setFields({
+      ...fields,
+      [inputName]: e.target.value
+    })
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    setStatus('loading')
+
+    const loginReq = await fetch('/api/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(fields),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+
+    if (!loginReq.ok) return setStatus('error ' + loginReq.status)
+
+    const loginRes = await loginReq.json()
+    console.log(loginRes)
+
+    setStatus('success')
+  }
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <input name='email' onChange={handleChange} type="email" placeholder='email' />
+        <input name='password' onChange={handleChange} type="password" placeholder="password" />
+        <button type='submit'>Login</button>
+        <div>status: {status}</div>
+      </form>
+    </>
+  )
+}
---
+++ pages/auth/register.js
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+export default function Register() {
+
+  const [fields, setFields] = useState({
+    email: '',
+    password: ''
+  })
+  const [status, setStatus] = useState('normal')
+
+  const handleRegister = async (e) => {
+    e.preventDefault()
+
+    setStatus('loading')
+
+    const registerReq = await fetch('/api/auth/register', {
+      method: 'POST',
+      body: JSON.stringify(fields),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (!registerReq.ok) return setStatus('error ' + registerReq.status)
+
+    console.log(registerReq)
+    const registerRes = await registerReq.json()
+    setStatus('success')
+  }
+
+  const handleChange = (e) => {
+    const inputName = e.target.getAttribute('name')
+    setFields({
+      ...fields,
+      [inputName]: e.target.value
+    })
+  }
+
+  return (
+    <>
+      <h1>Register Form</h1>
+      <form onSubmit={handleRegister}>
+        <input name='email' onChange={handleChange} type="email" placeholder="email" /><br />
+        <input name='password' onChange={handleChange} type="password" placeholder="password" /><br />
+        <button type="submit">Register</button><br />
+        <span>status {status}</span>
+      </form>
+    </>
+  )
+}
